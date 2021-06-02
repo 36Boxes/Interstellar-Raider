2356b031d899b59d584a069512446475d8d2fdb9
36Boxes <46636974+36Boxes@users.noreply.github.com>
2021-06-02 12:43:03 +0100

    Started refactoring

--- Interstellar Raider/GameScene.swift
+++ Interstellar Raider/GameScene.swift
@@ -7,103 +7,193 @@
 
 import SpriteKit
 import GameplayKit
+import GameKit
 
-class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+var userScore = 0
 
-        self.lastUpdateTime = 0
+class GameScene: SKScene , SKPhysicsContactDelegate{
+    
+    // Levels and user lives
+    
+    var levelNumber = 0
+    var lives = 3
+    
+    // SpriteKit Labels & nodes
+    
+    let ScoreLabel = SKLabelNode(fontNamed: "ADAM.CGPRO")
+    let LivesLabel = SKLabelNode(fontNamed: "ADAM.CGPRO")
+    let background = SKSpriteNode(imageNamed: "glitter-universe-1-1")
+    var enemy: SKSpriteNode!
+    let player = SKSpriteNode(imageNamed: "Boost1")
+    
+    
+    // Rocket Mode cases
+    
+    enum gameState{
+        case PreGame
+        case DuringGame
+        case GameFinished
+    }
+    
+    enum RocketMode {
+        case Boosted
+        case Normal
+        case DoubleXP
+        case BoostedDoubleXP
+    }
+    
+    var CurrentRocketMode = RocketMode.Normal
+
+    
+    // Physics categories for different nodes that appear on the screen
+    
+    struct PhysicsCatergories {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        static let None: UInt32 = 0
+        
+        static let Player: UInt32 = 1
+        
+        static let Bullet: UInt32 = 2
+        
+        static let Enemy: UInt32 = 4
+        
+        static let Asteroid: UInt32 = 8
+        
+        static let DoublePoints: UInt32 = 16
+        
+        static let GoldCoin: UInt32 = 32
+        
+        static let AsteroidFragment: UInt32 = 64
+        
+        static let GoldAsteroid: UInt32 = 128
+        
+        static let XPCoin: UInt32 = 256
+        
+        static let PurpleDiamond: UInt32 = 512
+        
+        static let GreenDiamond: UInt32 = 1024
+        
+        static let BlueDiamond: UInt32 = 2048
+    }
+    
+    // Texture atlas and array configuration
+    
+    var EnemyTextureAtlas = SKTextureAtlas()
+    var EnemyTextureArray: [SKTexture] = []
+    
+    var BlueDiamondTextureAtlas = SKTextureAtlas()
+    var BlueDiamondTextureArray: [SKTexture] = []
+    
+    var GreenDiamondTextureAtlas = SKTextureAtlas()
+    var GreenDiamondTextureArray: [SKTexture] = []
+    
+    var PurpleDiamondTextureAtlas = SKTextureAtlas()
+    var PurpleDiamondTextureArray: [SKTexture] = []
+    
+    var PlayerShipTextureAtlas = SKTextureAtlas()
+    var PlayerShipTextureArray: [SKTexture] = []
+    
+    var BackgroundTextureAtlas = SKTextureAtlas()
+    var BackgroundTextureArray: [SKTexture] = []
+    
+    var BoostedBackgroundTextureAtlas = SKTextureAtlas()
+    var BoostedBackgroundTextureArray: [SKTexture] = []
+    
+    var DoublePointsBackgroundTextureAtlas = SKTextureAtlas()
+    var DoublePointsBackgroundTextureArray: [SKTexture] = []
+    
+    var BoostedDoublePointsBackgroundTextureAtlas = SKTextureAtlas()
+    var BoostedDoublePointsBackgroundTextureArray: [SKTexture] = []
+    
+    // Game Area calculation, this isnt my code and i think it can be improved upon however it is not a priority as it works
+    
+//    let gameArea: CGRect
+//    
+//    override init(size: CGSize){
+//        
+//        // to have this work nicely on most devices i could maybe identify the device and then have the correct aspect ratio
+//        let maxAspectRatio: CGFloat = 19.5/9
+//        let gameAreaWidth = size.height / maxAspectRatio
+//        let margin = (size.width - gameAreaWidth) / 2
+//        gameArea = CGRect(x: margin, y: 0, width: gameAreaWidth, height: size.height)
+//        super.init(size:size)
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
+    override func didMove(to view: SKView) {
+        
+        userScore = 0
+        
+        self.physicsWorld.contactDelegate = self
+        loadTextures()
+        
+        background.size = CGSize (width: frame.maxX, height: frame.maxY)
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        self.addChild(background)
+        
+    }
+
+    
+    
+    
+    func loadTextures(){
+
+        PlayerShipTextureAtlas = SKTextureAtlas(named: "Boost.atlas")
+        BackgroundTextureAtlas = SKTextureAtlas(named: "BackgroundImages.atlas")
+        BoostedBackgroundTextureAtlas = SKTextureAtlas(named: "BoostedBackground.atlas")
+        DoublePointsBackgroundTextureAtlas = SKTextureAtlas(named: "DoublePointsBackground.atlas")
+        BoostedDoublePointsBackgroundTextureAtlas = SKTextureAtlas(named: "BoostedDoublePointsBackground.atlas")
+        PurpleDiamondTextureAtlas = SKTextureAtlas(named: "purpleDiamond.atlas")
+        GreenDiamondTextureAtlas = SKTextureAtlas(named: "greenDiamond.atlas")
+        BlueDiamondTextureAtlas = SKTextureAtlas(named: "blueDiamond.atlas")
+        EnemyTextureAtlas = SKTextureAtlas(named: "Enemyflames.atlas")
+        
+        for i in 1...EnemyTextureAtlas.textureNames.count{
+            let TexName = "E\(i).png"
+            EnemyTextureArray.append(SKTexture(imageNamed: TexName))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for n in 1...PlayerShipTextureAtlas.textureNames.count{
+            let texture = "Boost\(n).png"
+            PlayerShipTextureArray.append(SKTexture(imageNamed: texture))
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        for a in 1...PurpleDiamondTextureAtlas.textureNames.count{
+            let texture = "Purple\(a).png"
+            PurpleDiamondTextureArray.append(SKTexture(imageNamed: texture))
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
+        for a in 1...GreenDiamondTextureAtlas.textureNames.count{
+            let texture = "Green\(a).png"
+            GreenDiamondTextureArray.append(SKTexture(imageNamed: texture))
         }
         
-        self.lastUpdateTime = currentTime
+        for a in 1...BlueDiamondTextureAtlas.textureNames.count{
+            let texture = "Blue\(a).png"
+            BlueDiamondTextureArray.append(SKTexture(imageNamed: texture))
+        }
+        
+        for p in 1...BackgroundTextureAtlas.textureNames.count{
+            let texture = "glitter-universe-1-\(p).png"
+            BackgroundTextureArray.append(SKTexture(imageNamed: texture))
+        }
+        for p in 1...BoostedBackgroundTextureAtlas.textureNames.count{
+            let texture = "red-universe-1-\(p).png"
+            BoostedBackgroundTextureArray.append(SKTexture(imageNamed: texture))
+        }
+        for p in 1...DoublePointsBackgroundTextureAtlas.textureNames.count{
+            let texture = "green-universe-1-\(p).png"
+            DoublePointsBackgroundTextureArray.append(SKTexture(imageNamed: texture))
+        }
+        for p in 1...BoostedDoublePointsBackgroundTextureAtlas.textureNames.count{
+            let texture = "gold-universe-1-\(p).png"
+            BoostedDoublePointsBackgroundTextureArray.append(SKTexture(imageNamed: texture))
+        }
+        
     }
+
 }
